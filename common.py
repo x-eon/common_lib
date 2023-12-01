c12a74e5c109c1200734b3b7a771f6f4a2a66855
Игорь <39418662+xeonprog@users.noreply.github.com>
2023-12-01 14:12:00 +0400

    add time class

--- common.py
+++ common.py
@@ -1,5 +1,68 @@
 import matplotlib.pyplot as plt
 from datetime import datetime
+import time
+
+
+class Common:
+
+    def ViewCounter(self, period, idx):
+        per = period * 1.0
+        if (idx / per) - int(idx / per) == 0:
+            return True
+        return False
+
+    def cicle_index(self, idx, maxidx):
+        res = idx
+        if idx >= maxidx:
+            mn = int(idx / maxidx)
+            res = int(idx - mn * maxidx)
+        return res
+
+class Time:
+    def timeNew(self):
+        return datetime.now().strftime("%d-%m-%Y %H:%M:%S")
+    
+    def StepHour(self, secs):
+        return round(60 / (secs / 60), 2)
+
+    # 1 step seconds.
+    def sec_one_step(self, tmin, epochs):
+        res = (tmin * 60) / epochs
+        return res
+
+    def start_time(self):
+        return time.time()
+
+    def timeNew(self):
+        return datetime.now().strftime("%d-%m-%Y %H:%M:%S")
+
+    def timeToStr(self, _datetime):
+        return _datetime.strftime("%d-%m-%Y %H:%M:%S")
+
+    def timeToStrMQL(self, _datetime):
+        return _datetime.strftime('%Y.%m.%d %H:%M:%S')
+
+    def strToTime(self, _strtime):
+        return datetime.strptime(_strtime, "%Y.%m.%d %H:%M")
+
+    def spent_time(self, name, start_tm):
+        end_time = time.time()
+        run_time = (end_time - start_tm) / 60
+        spen_tm = round(run_time, 2)
+        print(name + ' TM:', spen_tm, 'min.')
+        return spen_tm
+
+    def calc_time(self, start_tm, _epochs):
+        tmin = self.spent_time('learning', start_tm)
+        secstep = round(self.sec_one_step(tmin, _epochs),4)
+        h1 = self.StepHour(secstep)
+        return secstep, h1
+
+    def difference_time(self, read_time, cur_time):
+        r_time = read_time.timestamp()
+        c_time = cur_time.timestamp()
+        return c_time - r_time
+
 
 class Convert:
 
